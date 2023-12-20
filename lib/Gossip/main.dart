b6212c6c7d575f81d00ba937aec6560c9126e127
Gizem <129165373+zehragizem@users.noreply.github.com>
2023-12-20 15:05:52 +0300

    Update main.dart

--- lib/Gossip/main.dart
+++ lib/Gossip/main.dart
@@ -15,8 +15,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(),
+      home: SplashPage(),
     );
+  }
+}
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds:2),
+    );
+
+    _animationController.forward(); // Start the animation
+
+    Future.delayed(Duration(seconds: 2), () {
+      _animationController.reset(); // Reset the animation
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyHomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 144, 223),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset(
+                (_animationController.value *
+                    MediaQuery.of(context).size.width) -
+                    MediaQuery.of(context).size.width / 2,
+                0.0,
+              ),
+              child: child,
+            );
+          },
+          child: Image.asset(
+            'images/witch_image.png',
+            width: 300.0,
+            height: 300.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
 
