b267f73678cf917c862d0c3597d13d91e24c5bf9
Gizem <129165373+zehragizem@users.noreply.github.com>
2023-12-20 10:25:23 +0300

    gizem

---
+++ lib/Gossip/model/imageDescription.dart
@@ -0,0 +1,11 @@
+class description{
+  static List<String> imageDescriptions = [
+    'Beautiful Girl',
+    'Christmas Decorations',
+    'Snowboarding',
+    'University Library',
+    'Cat',
+    'Snow',
+
+  ];
+}
---
+++ lib/Gossip/model/textMessage.dart
@@ -0,0 +1,13 @@
+class textm {
+  static List<String> textMessages = [
+    "Masalı, öykü ve destan gibi türlerden ayıran en önemli temel özellik ise gerçek dışı ögeler içermesi ve anlatılanları gerçeğe benzetme çabası içinde olmamasıdır.[1] Masallarda olaylar bilinmeyen bir zamanda geçer. Türkçede, bilinmeyen geçmiş zamana en uygun kip öğrenilen geçmiş zaman olduğundan, tüm masallar genellikle bu kiple anlatılır, görülen geçmiş zaman kipi ise hiç kullanılmaz, fakat bazen şimdiki zaman veya geniş zaman kipi kullanılır.",
+    'In the vast tapestry of the universe, each of us is a unique thread, weaving a story that is uniquely our own.Life is a journey, and every moment is a step into the unknown, filled with opportunities and challenges waiting to be embraced.Amidst the chaos of existence, find the serenity within your soul, and let it guide you through the storms and sunsets of life.Dreams are the whispers of the soul, urging us to chase the extraordinary and create a reality that mirrors the beauty within us.As the sun sets on today, let gratitude fill your heart for the moments that made you smile, the lessons that made you wiser, and the journey that made you stronger.',
+    'Embrace the symphony of life, with its highs and lows, for each note adds depth to the melody that is uniquely yours.',
+    'Dance with the rhythm of uncertainty, for it is in the unknown that the magic of discovery unfolds, revealing the extraordinary in the ordinary.',
+    'Like a star in the night sky, your presence has the power to illuminate the darkness and create a constellation of hope and inspiration.',
+    'In the garden of life, plant seeds of kindness, cultivate compassion, and let the flowers of love bloom in the fertile soil of your heart.Every sunrise is a blank canvas, inviting you to paint the day with the colors of your dreams, the brushstrokes of your actions, and the masterpiece of your journey.Life is a mosaic of moments, and with each piece, you have the power to create a masterpiece that reflects the essence of your being.',
+
+
+  ];
+}
+
