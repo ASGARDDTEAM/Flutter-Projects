7b555222f19f3236d4f075cbf01c455028bd4a73
Ecem Hatice Özkan <96335125+ehatice@users.noreply.github.com>
2023-12-27 00:44:39 +0300

    kalıcı mesajlar ve beğeni butonu eklendi

--- lib/ChatterCraft/PageOne.dart
+++ lib/ChatterCraft/PageOne.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-
 class PageOne extends StatefulWidget {
+  final List<Tweet> tweets;
+  final Function(Tweet) onTweetSubmitted;
+
+  PageOne({required this.tweets, required this.onTweetSubmitted});
+
   @override
   _PageOneState createState() => _PageOneState();
 }
 
 class _PageOneState extends State<PageOne> {
-  List<String> tweets = [];
-  final tweetController = TextEditingController();
+  final TextEditingController tweetController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +20,15 @@ class _PageOneState extends State<PageOne> {
       children: [
         Expanded(
           child: ListView.builder(
-            itemCount: tweets.length,
+            itemCount: widget.tweets.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(tweets[index]),
+              return TweetCard(
+                tweet: widget.tweets[index],
+                onLikePressed: () {
+                  setState(() {
+                    widget.tweets[index].toggleLike();
+                  });
+                },
               );
             },
           ),
@@ -35,26 +43,83 @@ class _PageOneState extends State<PageOne> {
                   decoration: InputDecoration(
                     hintText: 'What\'s happening?',
                   ),
-                  onFieldSubmitted: (tweet) {
-                    setState(() {
-                      tweets.add(tweet);
-                      tweetController.clear();
-                    });
+                  onFieldSubmitted: (tweetText) {
+                    final newTweet = Tweet(text: tweetText);
+                    widget.onTweetSubmitted(newTweet);
+                    tweetController.clear();
                   },
                 ),
               ),
               IconButton(
                 icon: Icon(Icons.send),
                 onPressed: () {
-                  setState(() {
-                    tweets.add(tweetController.text);
-                  });
+                  final newTweet = Tweet(text: tweetController.text);
+                  widget.onTweetSubmitted(newTweet);
+                  tweetController.clear();
                 },
               ),
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class Tweet {
+  final String text;
+  int likes = 0;
+  bool isLiked = false;
+
+  Tweet({required this.text});
+
+  void toggleLike() {
+    isLiked = !isLiked;
+    if (isLiked) {
+      likes++;
+    } else {
+      likes--;
+    }
+  }
+}
+
+class TweetCard extends StatelessWidget {
+  final Tweet tweet;
+  final VoidCallback onLikePressed;
+
+  TweetCard({required this.tweet, required this.onLikePressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(tweet.text),
+            SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        tweet.isLiked ? Icons.favorite : Icons.favorite_border,
+                        color: tweet.isLiked ? Colors.red : null,
+                      ),
+                      onPressed: onLikePressed,
+                    ),
+                    Text('${tweet.likes} Likes'),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/ChatterCraft/home_view.dart
+++ lib/ChatterCraft/home_view.dart
@@ -3,50 +3,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project/ChatterCraft/PageOne.dart';
 
 
-
-
-
 class HomeView extends StatefulWidget {
   @override
   _HomeViewState createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
+  List<Tweet> tweets = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: DefaultTabController(
-                 length: 2,
-                 child: Column(
-                 children: <Widget>[
-        Container(
-          child:AppBar(
-            title: Text("C H A T T E R C R A F T"),
-            centerTitle: true,
-            backgroundColor: Color.fromRGBO( 97, 169, 251, 1.0 ),
+          length: 2,
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: AppBar(
+                  title: Text("C H A T T E R C R A F T"),
+                  centerTitle: true,
+                  backgroundColor: Color.fromRGBO(97, 169, 251, 1.0),
+                ),
+                height: 50,
               ),
-            height:50 ,
-            ),
-        TabBar(
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.home),),
-                Tab(icon: Icon(Icons.flash_on),
-                )
+              TabBar(
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.home)),
+                  Tab(
+                    icon: Icon(Icons.flash_on),
+                  ),
+                ],
+                labelColor: Color.fromRGBO(97, 169, 251, 1.0),
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: <Widget>[
+                    PageOne(
+                      tweets: tweets,
+                      onTweetSubmitted: (Tweet tweet) {
+                        setState(() {
+                          tweets.add(tweet);
+                        });
+                      },
+                    ),
+                    PageOne(
+                      tweets: tweets,
+                      onTweetSubmitted: (Tweet tweet) {
+                        setState(() {
+                          tweets.add(tweet);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ],
-            labelColor: Color.fromRGBO( 97, 169, 251, 1.0 ),
-            ),
-            Expanded(
-              child: TabBarView(
-                children: <Widget>[
-                  PageOne(),
-                  PageOne(),
-                ],)
-              )
-          ],
+          ),
         ),
       ),
-      ),
-      );
+    );
   }
 }
