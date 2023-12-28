20531ee49e9c3ab734c1dd04ca06e06b55cc21c4
Gizem <129165373+zehragizem@users.noreply.github.com>
2023-12-28 15:53:39 +0300

    .

---
+++ lib/Duyurular/duyuru_main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:ikinci_sayfa/home.dart';
+
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home()
+    );
+  }
+} 
