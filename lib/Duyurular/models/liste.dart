9d484f5bd6ec4c007f6d70aea1941a8da7e2275e
Gizem <129165373+zehragizem@users.noreply.github.com>
2024-01-03 16:45:08 +0300

    .

--- lib/Duyurular/models/liste.dart
+++ lib/Duyurular/models/liste.dart
@@ -3,48 +3,78 @@ import 'package:flutter_project/Duyurular/models/liste_model.dart';
 class listem {
   static List<News> newsList = [
     News(
-      title: 'Sınav Takvimi',
-      year: 2023,
-      image: "images/download.jpg",
-      text:
-          "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-      tapCount: 0,
-    ),
-    News(
-      title: 'Burslar Açıklandı',
-      year: 2023,
-      image: "images/ayla.jpg",
-      text:
-          "Why do we use it?It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      tapCount: 0,
-    ),
-    News(
-      title: 'Lorem ipsum',
+      title: '2022-2023 Akademik Takvim Yayınlandı!',
       year: 2022,
-      image: "images/dune.jpg",
+      image: "images/akademiktakvim.jpg",
+      text:"Sayın Öğrencilerimiz,Yeni akademik yılına başlamak üzereyiz ve sizlere Bilgisayar Mühendisliği Bölümü Akademik Takvimini duyurmaktan memnuniyet duyuyoruz. Bu takvim, önemli tarihleri, ders programlarını, sınav dönemlerini ve diğer akademik etkinlikleri içermektedir.",
+      tapCount: 0,
+    ),
+    News(
+      title: 'KYK Bursları ve Krediler Açıklandı',
+      year: 2023,
+      image: "images/burs.jpg",
+      text:"Kredi ve Yurtlar Kurumu (KYK) olarak, yeni akademik dönemde öğrencilere yönelik sağladığımız burslarımızın sonuçları açıklandı. Heyecanla duyuruyoruz ki, KYK Bursları için başvurular değerlendirilmiş ve destek almaya hak kazanan öğrencilerimiz belirlendi.",
+      tapCount: 0,
+    ),
+    News(
+      title: "Öğrencilerimizin Geliştirdiği Mobil Uygulama Google Play'de",
+      year: 2022,
+      image: "images/uygulama2.jpg",
       text:
           "Where can I get some? There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
       tapCount: 0,
     ),
     News(
-      title: 'Stajlar Başladı',
+      title: 'Yaz Stajı Başvuruları Hakkında',
       year: 2023,
-      image: "images/joker.jpg",
+      image: "images/staj.jpg",
       text:
           "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,  comes from a line in section 1.10.32 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from  by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
       tapCount: 0,
     ),
     News(
-      title: 'Asgard',
-      year: 2021,
-      image: "images/thor.jpg",
+      title: 'Mini Otonom Araç Yarışmasında Öğrencilerimizin Başarısı',
+      year: 2023,
+      image: "images/basari.jpg",
       text:
           "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined ",
       tapCount: 0,
     ),
     News(
-      title: 'Yeni Yurt Açıldı',
-      year: 2020,
+      title: 'Trakya Kariyer Fuarı Etkinliği',
+      year: 2023,
+      image: "images/kariyer.jpg",
+      text:
+          "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
+      tapCount: 0,
+    ),
+    News(
+      title: 'TÜB Algoritma ve Programlama Hackatonu',
+      year: 2023,
+      image: "images/duyuru.jpg",
+      text:
+          "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
+      tapCount: 0,
+    ),
+    News(
+      title: 'OBS Güncellendi',
+      year: 2024,
+      image: "images/obs.jpg",
+      text:
+          "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
+      tapCount: 0,
+    ),
+    News(
+      title: 'Trakya Kariyer Fuarı Etkinliği',
+      year: 2023,
+      image: "images/duyuru.jpg",
+      text:
+          "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
+      tapCount: 0,
+    ),
+    News(
+      title: '2023-2024 Eğitim Öğretim Yılı Güz Dönemi Final Sınav Takvimi Açıklandı',
+      year: 2024,
       image: "images/takvim.jpg",
       text:
           "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
