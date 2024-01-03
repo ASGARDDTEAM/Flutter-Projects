3562de3a3aad9b21f8466b77da9ee9e3edb5ad45
sena <senaakat61@gmail.com>
2024-01-03 21:31:04 +0300

    expension tile was made

--- lib/ChatterCraft/Tweet/TweetCard.dart
+++ lib/ChatterCraft/Tweet/TweetCard.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_project/ChatterCraft/Tweet/Comment.dart';
 
 import 'Tweet.dart';
 
 class TweetCard extends StatelessWidget {
   final Tweet tweet;
   final VoidCallback onLikePressed;
+  final VoidCallback onReplyPressed;
 
-  TweetCard({required this.tweet, required this.onLikePressed});
+  TweetCard({required this.tweet, required this.onLikePressed, required this.onReplyPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +37,39 @@ class TweetCard extends StatelessWidget {
                     Text('${tweet.likes} Likes'),
                   ],
                 ),
+                IconButton(
+                  icon: Icon(Icons.quickreply),
+                  color: Color.fromARGB(255, 43, 0, 255),
+                  onPressed: onReplyPressed,
+                ),
               ],
             ),
+            SizedBox(height: 8.0),
+            if (tweet.comments.isNotEmpty)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ExpansionTile(
+                    backgroundColor: const Color.fromRGBO(221, 223, 253, 1.0),
+                    title: Text(
+                      'Yorumlar',
+                      style: TextStyle(color: Color.fromARGB(255, 130, 57, 208), fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    children: [
+                      for (var comment in tweet.comments)
+                        ListTile(
+                          leading: Icon(
+                            Icons.account_circle,
+                            color: Color.fromARGB(255, 130, 57, 208),
+                          ),
+                          title: Text(
+                            comment,
+                          ),
+                        ),
+                    ],
+                  ),
+                ],
+              ),
           ],
         ),
       ),
