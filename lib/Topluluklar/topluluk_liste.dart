cf0b9791a1bcc881c22840e6bb9e39cecb7a488c
iegul <101372918+iegul@users.noreply.github.com>
2024-01-04 11:04:38 +0300

    .

--- lib/Topluluklar/topluluk_liste.dart
+++ lib/Topluluklar/topluluk_liste.dart
@@ -4,7 +4,7 @@ class ToplulukMap {
       "image": "images/gdsc.png",
       "kisim":"GDSC",
       "uisim":"Google Developer Student Club",
-      "aciklama":"",
+      "aciklama":" Kuruluş Yılı:2019\n\nKuran Kişi: METU\n\nTopluluk hakkında kısa bilgi:dünya genelinde üniversite öğrencilerine teknoloji ve geliştirme konularında kaynaklar sağlayan, etkileşimli bir ortam sunan topluluklardır. Bu topluluklar, Google'ın teknoloji ürünleri ve platformları üzerindeki bilgi ve becerileri artırmayı amaçlar.\n\nÖrnek Etkinlikleri: Oyun Geceleri, Söyleşiler, Eğitimler(Unity 101, Blender 101, Android 101 ve Workshop vb.)",
     },
     "ieee": {
       "image": "images/ieee.png",
@@ -13,10 +13,10 @@ class ToplulukMap {
       "aciklama":"Kuruluş yılı:1884\n\nKuran kişi: Bilinmiyor( Alexander Graham Bell ve Thomas Edison gibi dönemin bilinen bilim insanlarınca atılmıştır.)\n\nTopluluk hakkında kısa bilgi: Elektrik ve Elektronik Mühendisleri Enstitüsü), elektrik, elektronik, bilgisayar, otomasyon, telekomünikasyon ve diğer birçok alanda mühendislik teori ve uygulamalarının gelişimi için çalışan, kâr amacı olmayan, dünyanın önde gelen teknik organizasyonudur.\n\nÖrnek Etkinlikleri:Code Night, Kariyer Konuşmaları, TRETEK"
     },
     "gdsc2": {
-      "image": "images/thunder.jpg",
-      "kisim":"GDSC2",
-      "uisim":"Google Developer Student Club",
-      "aciklama":"",
+      "image": "images/tema.jpg",
+      "kisim":"TEMA",
+      "uisim":" Genç Tema Topluluğu",
+      "aciklama":"Kuruluş Yılı:1992\n\nKuran Kişi:Hayrettin Karaca, Nihat Gökyiğit\n\n Topluluk Hakkında Kısa Bilgi: Erozyon ve çölleşme ile mücadele Türkiye'de acilen müdahale edilmesi gereken konular olmaya başlamıştı. Bu nedenle bu iki doğasever, konunun hassasiyetini iş dünyasının diğer liderlerine açtı ve 11 Eylül 1992 tarihinde Türkiye Erozyonla Mücadele, Ağaçlandırma ve Doğal Varlıkları Koruma Vakfı'nı kurdu.\n\nörnek Etkinlikler: Ağaç Dikme,Kar küresi yapımı, Doğa Yürüyüşü, Doğa Seminerleri",
     },
     "iot": {
       "image": "images/iot.jpg",
@@ -26,10 +26,10 @@ class ToplulukMap {
 
     },
     "gdsc4": {
-      "image": "images/thunder.jpg",
-      "kisim":"GDSC",
-      "uisim":"Google Developer Student Club",
-      "aciklama":"",
+      "image": "images/ymt.jpg",
+      "kisim":"YMT",
+      "uisim":"Yoga ve Meditasyon Topluluğu",
+      "aciklama":"Kuruluş yılı: 2023\n\nKuran kişi: Yogin İrem\n\nTopluluk hakkında kısa bilgi: Merhaba Zihin Yoldaşları! Huzur dolu bir dünyaya adım atın. Yoga ve Meditasyon Topluluğu, dinginlik arayan, zihinsel denge ve bedensel sağlık peşinde koşan herkesi bir araya getiren bir oluşumdur. Burada, farklı yoga teknikleri ve meditasyon pratiğini keşfetmek, birbirimizi desteklemek ve içsel huzuru bulmak için birlikte çalışmak amacıyla bir araya geliyoruz. Gel, birlikte nefes alalım, bedenimize ve ruhumuza odaklanalım!\n\nÖrnek Etkinlikler: Yoga Asanaları ve Nefes Çalışmaları, Meditasyon Geceleri, Doğa Yürüyüşleri, Ruhu Dinlendiren Çay Saatleri",
     },
     "baribe": {
       "image": "images/barbie.png",
@@ -39,10 +39,10 @@ class ToplulukMap {
 
     },
     "gdsc6": {
-      "image": "images/thunder.jpg",
-      "kisim":"GDSC",
-      "uisim":"Google Developer Student Club",
-      "aciklama":"",
+      "image": "images/ken.jpg",
+      "kisim":"MDCH",
+      "uisim":"Mojo Dojo Casa House",
+      "aciklama":"Kuruluş yılı:2009\n\nKuran kişi: Barbara Millicent Roberts (Ana Topluluk)\nMargot Robbie(Trakya Kolu)\n\nTopluluk hakkında kısa bilgi: \"Her şey olabilirsin\" sloganıyla küçük büyük demeden dünyadaki kadınların hepsinin kendilerini geliştirmeleri ve bulmaları amacıyla  Mattel desteğiyle kurulmuş topluluktur.2023 senesinde Margot Robbie'nin ricasıyla Trakya Üniversitesinde de bir kolumuz oluşturulmuştur. \n\nÖrnek Etkinlikleri:Geleceğe ışık ol, Kendini keşfet Workshop, Barbie Maratonları",
     },
     "anim": {
       "image": "images/anim.png",
@@ -51,10 +51,10 @@ class ToplulukMap {
       "aciklama":"Kuruluş yılı:2023\n\nKuran kişi: Team Asgardd \n\nTopluluk hakkında kısa bilgi:Fantastik ve animasyon filmlerini seven insanlar için kurulmuş topluluktur. Amacımız küçükken izlediğiniz çizgi filmler gibi aklınıza kazınan anılar üretmektir.\n\nÖrnek Etkinlikleri: Film Maratonları, Film Münazaraları, 2D ve 3D Modelleme-Animasyon Eğitimleri, Role play akşamları"
     },
     "gdsc8": {
-      "image": "images/thunder.jpg",
-      "kisim":"GDSC",
-      "uisim":"Google Developer Student Club",
-      "aciklama":"",
+      "image": "images/lotr.jpg",
+      "kisim":"YKT",
+      "uisim":"Yüzük Kardeşliği Topluluğu",
+      "aciklama":"Kuruluş yılı: 2018\n\nOrta Dünya'nın Kahramanı: Aragorn\n\nTopluluk hakkında kısa bilgi: Merhaba Orta Dünya Hayranları! Minas Tirith'e hoş geldiniz. Yüzük Kardeşliği, Tolkien'in epik eseri Yüzüklerin Efendisi'ne derin bir sevgi ve bağlılıkla bağlı olan bir topluluktur. Burada, Orta Dünya'nın zengin mitolojisi üzerine sohbet ediyor, Bilbo'nun hikayesinden Frodo'nun serüvenine kadar birçok önemli anı paylaşıyoruz. Etkinliklerimizde, Orta Dünya'nın tarihini keşfetmek, elf dillerini öğrenmek ve Sauron'un karanlık güçlerine karşı direnişimizi güçlendirmek için bir araya geliyoruz. Gel, bizimle birlikte Mordor'un gölgesine karşı aydınlığı savunmak için hazırlıklı ol!\n\nÖrnek Etkinlikler: Orta Dünya Haritası İncelemeleri, Yüzük Yolculuğu Film Maratonu, Elfçe Konuşma Atölyesi, Ork İmitasyon Yarışması",
     },
     "harry": {
       "image": "images/harry.png",
